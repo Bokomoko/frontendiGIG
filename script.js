e7a4ff642fe13358479d7fea04e7cffaa797e014
João Eurico Bokomoko Lima <34620239+Bokomoko@users.noreply.github.com>
2023-01-04 00:37:26 +0000

    Mudamos para a primeira versão do cliente (falta criar o formulário e as rotinas)

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-async function clicou() {
-  console.log("Vai dar o fetch")
-  const data = await fetch("https://servidorigig.bokomoko.repl.co/")
-  const resposta = await data.text()
-  console.log({resposta})
-   
+async function salvar() {
+  // pegar os dados do formulário html
+  // criar um objeto com os dados obtidos 
+  // tentar fazer uma solicitação de cadastro e enviar os dados
+  // se a tentativa der certo, notificar o usuário e limpar tela
+  // se a tentativa der errado, notificar o usuário e não limpar tela 
+  
+}
+
+function cancelar() {
+  // limpar os dados do formulário
 }
